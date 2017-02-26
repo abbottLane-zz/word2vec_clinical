42a114510388a3214e375345c8d99d89e724de40
wlane <will@asklane.com>
2017-02-26 09:58:18 -0800

    added skeleton script for online learning with existing models

--- train_online_word2vec.py
+++ train_online_word2vec.py
@@ -1,23 +1,30 @@
+import os
+import re
+
 import gensim
 
 
 def main():
     model_path = "/home/wlane/PycharmProjects/word2vec/Resources/Training_Output/mimic_fh12k.word2vec.model"
     new_docs_dir = ""
-    new_sentences = get_sentences(new_docs_dir)
+    new_sentences = Sentence_Generator(new_docs_dir)
 
     model = gensim.models.Word2Vec.load('/tmp/mymodel')
     model.train(new_sentences)
 
 
-def get_sentences(dir):
-    """
-    Given a directory where clinical docs live, return a list of tokenized sentences
-    :param dir: Where a set of new documents live
-    :return: a list of tokenized sentences
-    """
-    sents = list()
-    return sents
+class Sentence_Generator(object):
+    def __init__(self, dirname):
+        self.dirname = dirname
+
+    def __iter__(self):
+        for fname in os.listdir(self.dirname):
+            for line in open(os.path.join(self.dirname, fname)):
+                # TODO: remove punctuation from words: 'ppd.' and 'ppd' should be the same thing
+                sent = line.split()
+                sent = [x.lower() for x in sent]
+                sent = [re.sub(r'[(){}<>,.?/:;]', '', x) for x in sent]
+                yield sent
 
 if __name__ == "__main__":
     main()
